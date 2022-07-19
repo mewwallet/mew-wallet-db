c68b0ead4363f5e22fffa92531a66f657fba2665
Misha Nikanorov <fobozsb@gmail.com>
2022-07-18 18:33:30 -0700

    feature/nftasset_improvements: `NFTAssetTrait`: added `displayValue` that handle display_type and prepared value; `NFTCollection.Schema` must not be `RawRepresentable`; `NFTAssetLastSale` old `price` renamed to `rawPrice`, new `price` - ready to use;

--- Sources/Models/Concrete/Models/subs/NFTAssetTrait.swift
+++ Sources/Models/Concrete/Models/subs/NFTAssetTrait.swift
@@ -8,7 +8,38 @@
 import Foundation
 import mew_wallet_ios_extensions
 
-public struct NFTAssetTrait: MDBXBackedObject, Equatable, Hashable {
+public struct NFTAssetTrait: MDBXBackedObject {
+  public enum DisplayType {
+    case string(String)
+    case number(String)
+    case percentage(Decimal)
+    case date(Date)
+    case unknown(String)
+    init(rawValue: String, value: String) {
+      switch rawValue.uppercased() {
+      case "STRING":
+        self = .string(value)
+      case "NUMBER":
+        self = .number(value)
+      case "PERCENTAGE":
+        guard let value = Decimal(wrapped: value, hex: false) else {
+          self = .unknown(value)
+          return
+        }
+        self = .percentage(value)
+      case "DATE":
+        guard let timeInterval = TimeInterval(value) else {
+          self = .unknown(value)
+          return
+        }
+        self = .date(Date(timeIntervalSince1970: timeInterval))
+      case "UNKNOWN":
+        self = .unknown(value)
+      default:
+        self = .unknown(value)
+      }
+    }
+  }
   public weak var database: WalletDB?
   var _chain: MDBXChain
   var _wrapped: _NFTAssetTrait
@@ -24,17 +55,20 @@ extension NFTAssetTrait {
   public var count: UInt64 { _wrapped.count }
   public var value: String { _wrapped.value }
   public var percentage: Decimal { Decimal(wrapped: _wrapped.percentage, hex: false) ?? .zero }
+  public var displayValue: DisplayType { DisplayType(rawValue: _wrapped.displayType, value: _wrapped.value) }
 }
 
 // MARK: - NFTStats + Hashable
-extension NFTAssetTrait {
+
+extension NFTAssetTrait: Hashable {
   public func hash(into hasher: inout Hasher) {
     hasher.combine(_wrapped)
   }
 }
+
 // MARK: - NFTStats + Equatable
 
-extension NFTAssetTrait {
+extension NFTAssetTrait: Equatable {
   public static func == (lhs: NFTAssetTrait, rhs: NFTAssetTrait) -> Bool {
     lhs._wrapped == rhs._wrapped
   }
