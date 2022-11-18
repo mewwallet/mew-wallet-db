10a6953c494770d62ca4ea39b066f453527e40b4
Misha Nikanorov <fobozsb@gmail.com>
2022-11-18 01:21:17 -0800

    feature/subscription-foboz: added explanation for `DateComponents` extension

--- Sources/Extensions/DateComponents+TrackerTime.swift
+++ Sources/Extensions/DateComponents+TrackerTime.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 extension DateComponents {
+  /// Specific method for `TrackerTime`
+  /// Since weekday is unstable and there's no convenient methods to parse them - we're using `day` as `weekday`
+  /// 08/2022 has 1-7 Monday-Sunday, which is good enough
+  /// - Parameter includeDay: include or hardcode day (useful for daily and weekly tracker)
+  /// - Returns: prepared tuple of `DateComponents` and `DateFormatter` for components
   func trackerTime(includeDay: Bool) -> (DateComponents, DateFormatter) {
     let locale = Locale(identifier: "en_US_POSIX")
     let timeZone: TimeZone = .current
