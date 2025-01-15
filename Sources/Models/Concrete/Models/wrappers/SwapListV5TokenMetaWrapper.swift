ede60703b15667e024a282ec906cd526d1f5bf67
Misha Nikanorov <fobozsb@gmail.com>
2025-01-14 16:59:49 -0800

    [feature/v5_swap]: v5 models

---
+++ Sources/Models/Concrete/Models/wrappers/SwapListV5TokenMetaWrapper.swift
@@ -0,0 +1,162 @@
+//
+//  File.swift
+//  mew-wallet-db
+//
+//  Created by Mikhail Nikanorov on 1/13/25.
+//
+
+import Foundation
+import SwiftProtobuf
+ 
+public struct SwapListV5TokenMetaWrapper: MDBXWrapperObject {
+  var _chain: MDBXChain
+  var _wrapped: _SwapListV5TokenMetaWrapper
+  public weak var database: (any WalletDB)?
+  
+  public init(jsonString: String, chain: MDBXChain) throws {
+    var options = JSONDecodingOptions()
+    options.ignoreUnknownFields = true
+    self._chain = chain
+    self._wrapped = try _SwapListV5TokenMetaWrapper(jsonString: jsonString, options: options)
+    if self._wrapped.d.isEmpty {
+      self._wrapped.d = [Int32](repeating: 18, count: self._wrapped.c.count)
+    }
+  }
+  
+  public init(jsonData: Data, chain: MDBXChain) throws {
+    self._chain = chain
+    var options = JSONDecodingOptions()
+    options.ignoreUnknownFields = true
+    self._wrapped = try _SwapListV5TokenMetaWrapper(jsonUTF8Data: jsonData, options: options)
+    if self._wrapped.d.isEmpty {
+      self._wrapped.d = [Int32](repeating: 18, count: self._wrapped.c.count)
+    }
+  }
+}
+
+// MARK: - SwapListV5TokenMetaWrapper + Properties
+
+extension SwapListV5TokenMetaWrapper {
+  
+  // MARK: - Properties
+  
+  public var tokens: [TokenMeta] {
+    return self._wrapped.c.enumerated().map { (index, chain) in
+      return TokenMeta(
+        chain: MDBXChain(rawValue: chain),
+        contractAddress: Address(self._wrapped.ca[index]),
+        name: self._wrapped.n,
+        symbol: self._wrapped.s,
+        decimals: self._wrapped.d[index],
+        icon: self._wrapped.i,
+        price: self._wrapped.p,
+        database: database
+      )
+    }
+  }
+
+  public var tokens_dexItems: [DexItem] {
+    self._wrapped.c.enumerated().map { (index, chain) in
+      return DexItem(
+        chain: MDBXChain(rawValue: chain),
+        contractAddress: self._wrapped.ca[index],
+        name: self._wrapped.n,
+        symbol: self._wrapped.s,
+        order: nil,
+        database: database
+      )
+    }
+  }
+  
+  public var featured_dexItems: [DexItem] {
+    guard !self._wrapped.f.isEmpty else { return [] }
+    return self._wrapped.c.enumerated().compactMap { (index, chain) -> DexItem? in
+      let featuredIndex = self._wrapped.f[index]
+      guard featuredIndex != -1 else { return nil }
+      return DexItem(
+        chain: MDBXChain(rawValue: chain),
+        contractAddress: self._wrapped.ca[index],
+        name: self._wrapped.n,
+        symbol: self._wrapped.s,
+        order: UInt16(clamping: featuredIndex),
+        database: database
+      )
+    }
+  }
+}
+
+// MARK: - SwapListV5TokenMetaWrapper + ProtoWrappedMessage
+
+extension _SwapListV5TokenMetaWrapper: ProtoWrappedMessage {
+  func wrapped(_ chain: MDBXChain) -> SwapListV5TokenMetaWrapper {
+    return SwapListV5TokenMetaWrapper(self, chain: chain)
+  }
+}
+
+// MARK: - SwapListV5TokenMetaWrapper + ProtoWrapper
+
+extension SwapListV5TokenMetaWrapper: ProtoWrapper {
+  init(_ wrapped: _SwapListV5TokenMetaWrapper, chain: MDBXChain) {
+    self._chain = chain
+    self._wrapped = wrapped
+    if self._wrapped.d.isEmpty {
+      self._wrapped.d = [Int32](repeating: 18, count: self._wrapped.c.count)
+    }
+  }
+}
+
+extension SwapListV5TokenMetaWrapper {
+  public static func array(fromJSONString: String) throws -> [Self] {
+    return try _SwapListV5TokenMetaWrapper.array(fromJSONString: fromJSONString).map({ $0.wrapped(.universal) })
+  }
+  
+  public static func array(fromJSONUTF8Data: Data) throws -> [Self] {
+    return try _SwapListV5TokenMetaWrapper.array(fromJSONUTF8Data: fromJSONUTF8Data).map({ $0.wrapped(.universal) })
+  }
+}
+
+public struct SwapListV5Wrapper: Sendable {
+  public let tokens: [TokenMeta]
+  public let tokens_dexItems: [DexItem]
+  public let featured_dexItems: [DexItem]
+  
+  public init(fromJSONString: String) throws {
+    let items = try _SwapListV5TokenMetaWrapper.array(fromJSONString: fromJSONString).map({ $0.wrapped(.universal) })
+    self.tokens = items.tokens//.flatMap { $0.tokens }
+    self.tokens_dexItems = items.tokens_dexItems
+    self.featured_dexItems = items.featured_dexItems
+  }
+  
+  public init(fromJSONUTF8Data: Data) throws {
+    let items = try _SwapListV5TokenMetaWrapper.array(fromJSONUTF8Data: fromJSONUTF8Data).map({ $0.wrapped(.universal) })
+    self.tokens = items.tokens
+    self.tokens_dexItems = items.tokens_dexItems
+    self.featured_dexItems = items.featured_dexItems
+  }
+}
+
+public extension Array where Element == SwapListV5TokenMetaWrapper {
+  var tokens: [TokenMeta] {
+    return self.flatMap { $0.tokens }
+  }
+  
+  var tokens_dexItems: [DexItem] {
+    var order: UInt16 = 0
+    return self
+      .flatMap {
+        $0.tokens_dexItems
+          .map { item in
+            var item = item
+            item.order = order
+            order &+= 1
+            return item
+          }
+      }
+  }
+  
+  var featured_dexItems: [DexItem] {
+    return self.flatMap {
+      $0.featured_dexItems
+    }
+  }
+}
